0527dea1c4eb9e447ffa91c12c42dca9ba4e1fca
KaremAridi <80266460+KaremAridi@users.noreply.github.com>
2021-11-17 23:00:42 +0200

    done

--- app/src/main/java/com/assignment/technews/MainActivity.java
+++ app/src/main/java/com/assignment/technews/MainActivity.java
@@ -37,10 +37,37 @@ public class MainActivity extends AppCompatActivity {
     SQLiteDatabase database;
     public static String articleTitle;
     public static String articleUrl;
-    int idIndex, titleIndex, urlIndex;
+    public static String articleID;
 
     //Step 1
 //    public class DownloadTask extends AsyncTask<String, Void, String> {
+//
+//        @Override
+//        protected String doInBackground(String... strings) {
+//            String result = "";
+//            URL url;
+//            HttpURLConnection urlConnection;
+//
+//            try {
+//                url = new URL(strings[0]);
+//                urlConnection = (HttpURLConnection) url.openConnection();
+//                InputStream inp = urlConnection.getInputStream();
+//                BufferedReader reader = new BufferedReader(new InputStreamReader(inp));
+//                String data = reader.readLine();
+//
+//                while (data != null) {
+//                    result += data;
+//                    data = reader.readLine();
+//                }
+//                return result;
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                return null;
+//            }
+//        }
+//    }
+//
+//    public class DownloadTask2 extends AsyncTask<String, Void, String> {
 //
 //        @Override
 //        protected String doInBackground(String... strings) {
@@ -72,15 +99,11 @@ public class MainActivity extends AppCompatActivity {
 //
 //            try {
 //                JSONObject json = new JSONObject(s);
+//                articleID = json.getString("id");
 //                articleTitle = json.getString("title");
 //                articleUrl = json.getString("url");
+//                database.execSQL("INSERT INTO articles(id,title,url) VALUES (\'" + articleID + "\',\'" + articleTitle + "\',\'" + articleUrl + "\')");
 //
-//                articleTitles.add(articleTitle);
-//
-//                //Creating DB
-//                database.execSQL("CREATE TABLE IF NOT EXISTS articles (id VARCHAR,title VARCHAR, url VARCHAR)");
-//
-//                database.execSQL("INSERT INTO articles(id,title,url) VALUES (articleID,articleTitle,articleUrl)");
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
@@ -97,8 +120,11 @@ public class MainActivity extends AppCompatActivity {
 //        //Initialize DB
 //        database = this.openOrCreateDatabase("articlesDB", MODE_PRIVATE, null);
 //
-//        DownloadTask task = new DownloadTask();
+//        //Creating DB
+//        //database.execSQL("DROP TABLE  articles");
+//        database.execSQL("CREATE TABLE IF NOT EXISTS articles (id VARCHAR, title VARCHAR, url VARCHAR)");
 //
+//        DownloadTask task = new DownloadTask();
 //        try {
 //            String res = task.execute("https://hacker-news.firebaseio.com/v0/topstories.json").get();
 //            String[] temp = res.split(",");
@@ -109,7 +135,7 @@ public class MainActivity extends AppCompatActivity {
 //
 //            //Inserting to DB
 //            for (int i = 0; i < 20; i++) {
-//                DownloadTask task2 = new DownloadTask();
+//                DownloadTask2 task2 = new DownloadTask2();
 //
 //                String articleID = articleIDs.get(i);
 //
@@ -123,35 +149,43 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
-
+//  Step2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myListView = (ListView) findViewById(R.id.newsView);
+        database = this.openOrCreateDatabase("articlesDB", MODE_PRIVATE, null);
+
+//        Creating cursor to choose
+        Cursor c = database.rawQuery("SELECT * FROM articles", null);
+
+        int titleIndex = c.getColumnIndex("title");
+        int urlIndex = c.getColumnIndex("url");
+        c.moveToFirst();
+
+        for(int i=0;i<20;i++){
+            //filling arrays from db
+            articleTitles.add(c.getString(titleIndex));
+            articleUrls.add(c.getString(urlIndex));
+            c.moveToNext();
+        }
+            c.close();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, articleTitles);
         myListView.setAdapter(adapter);
-
-        //Creating cursor to choose
-//        Cursor c = database.rawQuery("SELECT * FROM articles", null);
-//        idIndex = c.getColumnIndex("id");
-//        titleIndex = c.getColumnIndex("title");
-//        urlIndex = c.getColumnIndex("url");
-//        c.moveToFirst();
 
         //on click
         myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                c.moveToPosition(position);
-//                String articleTitletemp = c.getString(titleIndex);
-//                String articleUrltemp = c.getString(urlIndex);
-//
-//                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-//                intent.putExtra("title", articleTitletemp);
-//                intent.putExtra("url", articleUrltemp);
-//                MainActivity.this.startActivity(intent);
+
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+               intent.putExtra("title", articleTitles.get(position));
+               intent.putExtra("url", articleUrls.get(position));
+                MainActivity.this.startActivity(intent);
             }
         });
     }
+
 }
